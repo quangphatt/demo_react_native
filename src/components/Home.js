c88dfeef0f490fc968d1e100b3fa9733037ece08
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2022-04-11 15:49:03 +0700

    custom Bottom Tab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import Feed from './Feed';
 import Notifycation from './Notifycation';
+import Post from './Post';
 import Setting from './Setting';
 import Profile from './Profile';
+import Others from './Others';
 import About from './About';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
@@ -25,21 +27,24 @@ class HomeScreen extends Component {
       <Tab.Navigator
         screenOptions={{
           headerShown: false,
-          tabBarActiveTintColor: '#000',
-          tabBarInactiveTintColor: '#635d54',
-          tabBarActiveBackgroundColor: '#f2bf72',
-          tabBarInctiveBackgroundColor: '#fff',
+          tabBarShowLabel: false,
         }}>
         <Tab.Screen
           name="Feed"
           component={Feed}
           options={{
-            tabBarIcon: ({focused, color}) => (
-              <FontAwesome5
-                name={'home'}
-                size={20}
-                color={focused ? '#524c43' : '#696156'}
-              />
+            tabBarIcon: ({focused}) => (
+              <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <FontAwesome5
+                  name={'home'}
+                  size={21}
+                  color={focused ? '#000' : '#9c9c9c'}
+                />
+                <Text
+                  style={{color: focused ? '#000' : '#9c9c9c', fontSize: 9}}>
+                  Feed
+                </Text>
+              </View>
             ),
           }}
         />
@@ -47,12 +52,37 @@ class HomeScreen extends Component {
           name="Notifycation"
           component={Notifycation}
           options={{
-            tabBarIcon: ({focused, color}) => (
-              <FontAwesome5
-                name={'bell'}
-                size={20}
-                color={focused ? '#524c43' : '#696156'}
-              />
+            tabBarIcon: ({focused}) => (
+              <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <FontAwesome5
+                  name={'bell'}
+                  size={21}
+                  color={focused ? '#000' : '#9c9c9c'}
+                />
+                <Text
+                  style={{color: focused ? '#000' : '#9c9c9c', fontSize: 9}}>
+                  Notification
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Post"
+          component={Post}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View
+                style={{
+                  backgroundColor: focused ? '#009933' : '#33cc33',
+                  width: 40,
+                  height: 40,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: 20,
+                }}>
+                <FontAwesome5 name={'plus'} size={18} color={'#fff'} />
+              </View>
             ),
           }}
         />
@@ -60,25 +90,37 @@ class HomeScreen extends Component {
           name="Setting"
           component={Setting}
           options={{
-            tabBarIcon: ({focused, color}) => (
-              <FontAwesome5
-                name={'cog'}
-                size={20}
-                color={focused ? '#524c43' : '#696156'}
-              />
+            tabBarIcon: ({focused}) => (
+              <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <FontAwesome5
+                  name={'cog'}
+                  size={21}
+                  color={focused ? '#000' : '#9c9c9c'}
+                />
+                <Text
+                  style={{color: focused ? '#000' : '#9c9c9c', fontSize: 9}}>
+                  Setting
+                </Text>
+              </View>
             ),
           }}
         />
         <Tab.Screen
-          name="Profile"
-          component={Profile}
+          name="Others"
+          component={Others}
           options={{
-            tabBarIcon: ({focused, color}) => (
-              <FontAwesome5
-                name={'user'}
-                size={20}
-                color={focused ? '#524c43' : '#696156'}
-              />
+            tabBarIcon: ({focused}) => (
+              <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <FontAwesome5
+                  name={'align-justify'}
+                  size={21}
+                  color={focused ? '#000' : '#9c9c9c'}
+                />
+                <Text
+                  style={{color: focused ? '#000' : '#9c9c9c', fontSize: 9}}>
+                  Others
+                </Text>
+              </View>
             ),
           }}
         />
